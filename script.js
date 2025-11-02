19f02e50c811b07cfcfa6beb068fecb52964eec8
cactuzhead <cactuzhead@gmail.com>
2025-11-02 22:07:32 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -231,7 +231,7 @@ function setTheme(isDark) {
 function formatDate(str) {
     const date = new Date(str);
     const day = date.getDate();
-    const monthNames = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"];
+    const monthNames = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","November","Dec"];
     const month = monthNames[date.getMonth()];
     const year = date.getFullYear();
     return `${day} ${month} ${year}`;
