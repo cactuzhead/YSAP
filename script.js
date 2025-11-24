8fe8fbec4a4b46c4b030bed79b7683324b424245
cactuzhead <cactuzhead@gmail.com>
2025-11-24 14:24:03 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -518,38 +518,33 @@ function prepareTempCanvas() {
         const lw = brushSize;
         const color = drawColor.value || '#f94144';
 
-        const shouldShowCursor = ((erasing && drawMode.value === "erase") || drawMode.value === 'free');
+        const shouldShowCursor = ((erasing ) || drawMode.value === 'free');
         if (!shouldShowCursor) {
-            // eraserCursor.style.display = 'none';
-            // drawCanvas.style.cursor = '';
+            eraserCursor.style.display = 'none';
+            drawCanvas.style.cursor = '';
         } else {
-            // eraserCursor.style.display = 'block';
-            // eraserCursor.style.width = `${brushSize}px`;
-            // eraserCursor.style.height = `${brushSize}px`;
-            // eraserCursor.style.left = `${e.clientX}px`;
-            // eraserCursor.style.top = `${e.clientY}px`;
+            eraserCursor.style.display = 'block';
+            eraserCursor.style.width = `${brushSize}px`;
+            eraserCursor.style.height = `${brushSize}px`;
+            eraserCursor.style.left = `${e.clientX}px`;
+            eraserCursor.style.top = `${e.clientY}px`;
         }
 
         if (drawMode.value === 'free') {
             tempCtx.lineWidth = brushSize;
             tempCtx.lineCap = 'round';
 
-            if (erasing || drawMode.value === 'free') {
+            if (erasing) {
                 // Eraser mode
                 drawEraserCursor(p.x, p.y);
                 tempCtx.globalCompositeOperation = 'destination-out';
                 tempCtx.strokeStyle = 'rgba(0,0,0,1)';
                 eraserCursor.style.display = 'none';
-             drawCanvas.style.cursor = '';
+            drawCanvas.style.cursor = '';
             } else {
                 // Normal drawing
                 tempCtx.globalCompositeOperation = 'source-over';
                 tempCtx.strokeStyle = drawColor.value || '#f94144';
-                 eraserCursor.style.display = 'block';
-                eraserCursor.style.width = `${brushSize}px`;
-                eraserCursor.style.height = `${brushSize}px`;
-                eraserCursor.style.left = `${e.clientX}px`;
-                eraserCursor.style.top = `${e.clientY}px`;
             }
 
             tempCtx.beginPath();
